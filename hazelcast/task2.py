c9c53b23ee54d61573584b1fc4f20e5460facfce
Denys Herasymuk <42843889+denysgerasymuk799@users.noreply.github.com>
2022-03-17 23:47:58 +0200

    added task2

--- hazelcast/task2.py
+++ hazelcast/task2.py
@@ -1,11 +1,6 @@
-import time
 import hazelcast
 
 from multiprocessing import Process
-
-
-class Value:
-    amount = 0
 
 
 def racy_update(number):
@@ -14,41 +9,62 @@ def racy_update(number):
     # Create Distributed Map
     map = hz.get_map("distributed_map2").blocking()
     key = "1"
-    map.put(key, Value())
-
-    def put_callback(f):
-        return f.result()
+    map.put(key, 0)
 
     print(f"Process {number} -- Starting")
     for k in range(0, 1000):
         if k % 100 == 0:
             print(f"Process {number} -- At: {k}")
-        value = map.get(key).add_done_callback(put_callback)
-        time.sleep(0.01)
-        value.amount += 1
+
+        value = map.get(key)
+        value += 1
         map.put(key, value)
 
-    print(f"Process {number} -- Finished! Result = {map.get(key).amount}")
+    print(f"Process {number} -- Finished! Result = {map.get(key)}")
+
+
+def pessimistic_update(number):
+    hz = hazelcast.HazelcastClient()
+
+    # Get Distributed Map
+    map = hz.get_map("distributed_map2").blocking()
+    key = "1"
+    map.put(key, 0)
+
+    print(f"Process {number} -- Starting")
+    for k in range(0, 1000):
+        map.lock(key)
+        try:
+            value = map.get(key)
+            value += 1
+            map.put(key, value)
+        finally:
+            map.unlock(key)
+
+    print(f"Process {number} -- Finished! Result = {map.get(key)}")
 
 
 def optimistic_update(number):
     hz = hazelcast.HazelcastClient()
 
-    # Create Distributed Map
+    # Get Distributed Map
     map = hz.get_map("distributed_map2").blocking()
     key = "1"
-    map.put(key, Value())
+    map.put(key, 0)
 
     print(f"Process {number} -- Starting")
     for k in range(0, 1000):
-        if k % 100 == 0:
+        if k % 10 == 0:
             print(f"Process {number} -- At: {k}")
-        value = map.get(key)
-        time.sleep(0.01)
-        value.amount += 1
-        map.put(key, value)
 
-    print(f"Process {number} -- Finished! Result = {map.get(key).amount}")
+        while True:
+            old_value = map.get(key)
+            new_value = old_value
+            new_value += 1
+            if map.replace_if_same(key, old_value, new_value):
+                break
+
+    print(f"Process {number} -- Finished! Result = {map.get(key)}")
 
 
 if __name__ == '__main__':
@@ -56,7 +72,7 @@ if __name__ == '__main__':
     procs = []
 
     for index, number in enumerate(numbers):
-        proc = Process(target=racy_update, args=(number,))
+        proc = Process(target=pessimistic_update, args=(number,))
         procs.append(proc)
         proc.start()
 
